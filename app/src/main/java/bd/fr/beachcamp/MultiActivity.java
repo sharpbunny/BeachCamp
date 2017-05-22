ada61278f72e85a3f99d52b6ebe7d413edc3c2f1
BDDorian <boudrique.dorian@gmail.com>
2017-05-22 09:05:22 +0200

    Files added in the project

---
+++ app/src/main/java/bd/fr/beachcamp/MultiActivity.java
@@ -0,0 +1,13 @@
+package bd.fr.beachcamp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MultiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_multi);
+    }
+}
