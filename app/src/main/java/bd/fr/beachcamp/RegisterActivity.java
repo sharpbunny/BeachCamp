7ded3a0fb73477cbc43235009e6e80e6fca9f484
Vona <vona.chum@gmail.com>
2017-05-23 14:36:07 +0200

    Ajout qq fonctions sur multi

--- app/src/main/java/bd/fr/beachcamp/RegisterActivity.java
+++ app/src/main/java/bd/fr/beachcamp/RegisterActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -27,6 +28,16 @@ public class RegisterActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        if (passwordBox == passwordConfBox){
+            Toast.makeText(getApplicationContext(),
+                    "mdp OK",Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Toast.makeText(getApplicationContext(),
+                    "mdp NOK",Toast.LENGTH_SHORT).show();
+        }
+
 
     }
 }
